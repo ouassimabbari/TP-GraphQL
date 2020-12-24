eb6125943e6110cbcae56fb08d6bf784ea82486c
ouassimabbari <wassim.abbari@gmail.com>
2020-12-24 08:46:48 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const app = express()
+
+const authors = [
+    { id: 1, name: 'J. K. Rowling' },
+    { id: 2, name: 'J. R. R. Tolkien' },
+    { id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+    { id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+    { id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+    { id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+    { id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+    { id: 5, name: 'The Two Towers', authorId: 2 },
+    { id: 6, name: 'The Return of the King', authorId: 2 },
+    { id: 7, name: 'The Way of Shadows', authorId: 3 },
+    { id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+app.listen(5000, () => console.log('Server Running on port 5000'))
